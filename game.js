6c03cd5188e849b37ddcad280267c6e83c71c917
vikramadditya <86791011+vikramadditya@users.noreply.github.com>
2021-09-07 13:58:42 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,38 @@
+ function send (){
+     number1 = document.getElementById ("number1").value;
+     number2 = document.getElementById ("number2").value;
+     question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row =  question_number + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+	document.getElementById("number1").value = "";
+	document.getElementById("number2").value = "";
+}
+function check()
+{
+	get_answer = document.getElementById("input_check_box").value;
+	if(get_answer == actual_answer)	
+	{
+		if(answer_turn == "player1")
+		{
+			player1_score = player1_score +1;
+		    document.getElementById("player1_score").innerHTML = player1_score;
+		}
+		else 
+		{
+			player2_score = player2_score +1;
+		    document.getElementById("player2_score").innerHTML = player2_score;
+		}
+	}
+	
+	if(question_turn == "player1")
+	{
+		question_turn = "player2"
+		document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+	}
+	else 
+	{
+		question_turn = "player1"
+		document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+	}
